088b788acbc7d6815e86f7736c21b88255212428
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-01-16 14:47:11 +0300

    Made Component Repository

---
+++ src/Components/About-Us.js
@@ -0,0 +1,15 @@
+const AboutUs = () => {
+  return (
+    <div className="About-Us">
+      <h1>The Great Software Company</h1>
+
+      <p>
+      Great Software Company Company is a leading innovator in Software, dedicated to providing
+        high-quality products and exceptional customer service. With a focus on
+        sustainability and cutting-edge technology, we strive to create
+        solutions that drive progress and improve lives globally
+      </p>
+    </div>
+  );
+};
+export default AboutUs;
---
+++ src/Contact-Us.js
@@ -0,0 +1,8 @@
+const ContactUs =()=>{
+    return(
+        <div className="contact-us">
+            <h2>Fill The Form Below:</h2>
+        </div>
+    )
+}
+export default ContactUs;
